6d5e1d40a3ab41215c596475ad55c9f6f626c30b
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-12-28 12:16:58 -0800

    combine corporate funds and eventbrite funds into single value on leaderboard

--- pages/models.py
+++ pages/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=255, null=True, blank=True)
-    funding = models.FloatField(max_length=10, null=True, blank=True)
+    eventbrite_funds = models.FloatField(max_length=10, null=True, blank=True)
+    corporate_funds = models.FloatField(max_length=10, null=True, blank=True)
 
     def __str__(self):
         return str(self.pk)
