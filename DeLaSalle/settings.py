b58b9a63db985c80597221a41e1475634f4b3ed6
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2018-01-01 14:34:50 -0800

    configuring settings for deploy

--- DeLaSalle/settings.py
+++ DeLaSalle/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -106,3 +111,34 @@ STATICFILE_DIRS = [os.path.join(BASE_DIR, "pages", "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'DeLaSalle',
+            'PASSWORD': LOCAL_PASSWORD,
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dlsnc.fundraiser@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PW']
+EMAIL_PORT = 587
+
+TOKEN = os.environ['TOKEN']
